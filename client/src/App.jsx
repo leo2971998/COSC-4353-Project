9e519102f6b7d2ec936ac83f6939abd57a2ef86d
Sthiber <sthiber133guevara@gmail.com>
2025-06-30 16:32:18 -0500

    Created the landing page and added it to the router as a public route, the template landing page is now functional and up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Routes>
         {/* Place Public Routes Here */}
-        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </Router>
   );
