3e4b5b988ff2f64a9bb46da8303ce04c3419fa93
Sthiber <sthiber133guevara@gmail.com>
2025-06-30 12:13:25 -0500

    Installed lucide react for icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./index.css";
 
 function App() {
   return (
-    <>
-      <div className="text-3xl font-bold text-blue-500">
-        Tailwind is working!
-      </div>
-
-      <div>
-        <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700">
-          Test Button
-        </button>
-      </div>
-    </>
+    <Router>
+      {/* Place Public Routes Here */}
+      {/* <Route path="/" element={<HomePage />} /> */}
+    </Router>
   );
 }
 
