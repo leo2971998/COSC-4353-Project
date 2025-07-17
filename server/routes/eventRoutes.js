a6bf89238ef6dab3dc07e8d1478b63cdc49a3159
Sthiber <sthiber133guevara@gmail.com>
2025-07-17 15:25:08 -0500

    fix: path bug causing server to not launch.

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createEvent } from "./controllers/eventControllers.js";
+import { createEvent } from "../controllers/eventController.js";
 
 const router = express.Router();
 
