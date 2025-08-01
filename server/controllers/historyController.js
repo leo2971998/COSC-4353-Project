c88248846330589d63dc1ca78ab2c6bc368a6733
Sthiber <sthiber133guevara@gmail.com>
2025-08-01 03:11:46 -0500

    feat: added the GET request in the backend which gets all of the user's history from the database.

--- server/controllers/historyController.js
+++ server/controllers/historyController.js
@@ -1,5 +1,22 @@
 import { volunteerHistory } from "../data/volunteerHistory.js";
+import { query } from "../db.js";
 
 export const getVolunteerHistory = (req, res) => {
   res.json(volunteerHistory);
+};
+
+export const getVolunteerHistoryParams = async (req, res) => {
+  try {
+    const volunteer_id = req.params.userID;
+    const sql =
+      "SELECT vh.history_id, em.event_name, em.event_description, em.event_location, em.skills, em.start_time, vh.event_status, em.urgency FROM volunteer_history as vh JOIN eventManage as em ON vh.event_id = em.event_id WHERE vh.volunteer_id = ?;";
+    const volunteer_history = await query(sql, [volunteer_id]);
+
+    res.status(200).json({ volunteer_history });
+  } catch (error) {
+    console.error("Error fetching volunteer's history ", error);
+    res
+      .status(500)
+      .json({ message: "Server error fetching volunteer's history" });
+  }
 };
--- server/routes/historyRoutes.js
+++ server/routes/historyRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getVolunteerHistory } from "../controllers/historyController.js";
+import {
+  getVolunteerHistory,
+  getVolunteerHistoryParams,
+} from "../controllers/historyController.js";
 
 const router = express.Router();
 
 router.get("/", getVolunteerHistory);
+router.get("/:userID", getVolunteerHistoryParams);
 
 export default router;
