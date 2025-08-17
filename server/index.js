b1a0a8c20a6299842cb3a993016b241ed5e651fd
Leo Nguyen <106453722+leo2971998@users.noreply.github.com>
2025-08-17 15:52:17 -0500

    Delete server directory

--- server/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./server.js";
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
